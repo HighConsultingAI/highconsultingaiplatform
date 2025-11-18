18eaca35eec957e3060c16f1fc400832b52628f5
HighConsultingAI <highconsultingai@gmail.com>
2025-11-18 18:41:14 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
-```js
-// app/page.js - redirect to /workflows
 import { redirect } from 'next/navigation'
+
 export default function Page() {
-redirect('/workflows')
+  redirect('/workflows')
 }
-```
