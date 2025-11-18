bdf057e8e275c6f7dbbd049a359c7f8316e2c9cc
HighConsultingAI <highconsultingai@gmail.com>
2025-11-18 15:22:40 -0500

    Update page.jsx

--- app/workflows/[id]/page.jsx
+++ app/workflows/[id]/page.jsx
@@ -2,7 +2,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import CanvasArea from '../../components/CanvasArea'
+import CanvasArea from '@/components/CanvasArea'
 
 export default function WorkflowEditor({ params }) {
   const { id } = params
