c32a68b6f5db90f93d83059021082d3b76a059c3
HighConsultingAI <highconsultingai@gmail.com>
2025-11-19 17:51:09 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+  experimental: {
+    // keep defaults; you can remove experimental keys if unused
+  }
+}
+module.exports = nextConfig
